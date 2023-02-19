0f7cc7ac2f7361adc5df4ad0c70a852e7e8aa52e
Nathaniel Tampus <contact@nize.ph>
2023-02-19 19:39:44 +0800

    add custom daisyUI themes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,36 @@ module.exports = {
     },
     plugins: [require("daisyui")],
     daisyui: {
-        themes: ["cmyk", "night"],
-        darkTheme: "night"
+        // based on daisyUI night and winter themes
+        themes: [
+            {
+                chessuLight: {
+                    primary: "#047AFF",
+                    secondary: "#463AA2",
+                    accent: "#C148AC",
+                    neutral: "#021431",
+                    "base-100": "#FFFFFF",
+                    "base-200": "#F2F7FF",
+                    "base-300": "#E3E9F4",
+                    "base-content": "#394E6A",
+                    info: "#93E7FB",
+                    success: "#81CFD1",
+                    warning: "#EFD7BB",
+                    error: "#E58B8B"
+                },
+                chessuDark: {
+                    primary: "#38BDF8",
+                    secondary: "#818CF8",
+                    accent: "#1d4ed8",
+                    neutral: "#1E293B",
+                    "base-100": "#0F172A",
+                    info: "#0CA5E9",
+                    success: "#2DD4BF",
+                    warning: "#F4BF50",
+                    error: "#FB7085"
+                }
+            }
+        ],
+        darkTheme: "chessuDark"
     }
 };
