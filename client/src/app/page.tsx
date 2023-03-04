dd0a097a4877610edcf868ee5b40b466c5324b84
Nathaniel Tampus <contact@nize.ph>
2023-03-04 18:43:32 +0800

    initial find &  create game functionality and restructure homepage components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,6 @@
-import PublicGames from "@/components/PublicGames";
+import PublicGames from "@/components/home/PublicGames";
+import JoinGame from "@/components/home/JoinGame";
+import CreateGame from "@/components/home/CreateGame";
 
 export default function Home() {
   return (
@@ -8,39 +10,14 @@ export default function Home() {
       <div className="flex flex-col items-center gap-4">
         <div className="flex flex-col items-center">
           <h2 className="mb-4 text-xl font-bold leading-tight">Join from invite</h2>
-          <div className="form-control">
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Invite link or code"
-                className="input input-bordered"
-              />
-              <button className="btn btn-square">Join</button>
-            </div>
-          </div>
+          <JoinGame />
         </div>
 
         <div className="divider divider-vertical">or</div>
 
         <div className="flex flex-col items-center">
           <h2 className="mb-4 text-xl font-bold leading-tight">Create game</h2>
-          <div className="form-control">
-            <label className="label cursor-pointer">
-              <span className="label-text">Unlisted/invite-only</span>
-              <input type="checkbox" className="checkbox" />
-            </label>
-            <label className="label">
-              <span className="label-text">Select your side</span>
-            </label>
-            <div className="input-group">
-              <select className="select select-bordered">
-                <option value="random">Random</option>
-                <option value="white">White</option>
-                <option value="black">Black</option>
-              </select>
-              <button className="btn">Create</button>
-            </div>
-          </div>
+          <CreateGame />
         </div>
       </div>
     </div>
---
+++ client/src/components/home/JoinGame.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import type { FormEvent } from "react";
+import { useState, useContext } from "react";
+import { SessionContext } from "@/context/session";
+import { findGame } from "@/lib/game";
+import { useRouter } from "next/navigation";
+
+export default function JoinGame() {
+  const session = useContext(SessionContext);
+  const [buttonLoading, setButtonLoading] = useState(false);
+  const router = useRouter();
+
+  async function submitJoinGame(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!session?.user?.id) return;
+    setButtonLoading(true);
+
+    const target = e.target as HTMLFormElement;
+    const codeEl = target.elements.namedItem("joinGameCode") as HTMLInputElement;
+
+    let code = codeEl.value;
+    if (code.startsWith("http") || code.startsWith("ches.su")) {
+      code = new URL(code).pathname.split("/")[2];
+    }
+
+    const game = await findGame(code);
+
+    if (game) {
+      router.push(`/game/${game.code}`);
+    } else {
+      setButtonLoading(false);
+      codeEl.value = "";
+      // TODO: Show error message
+    }
+  }
+
+  return (
+    <form className="input-group" onSubmit={submitJoinGame}>
+      <input
+        type="text"
+        placeholder="Invite link or code"
+        className="input input-bordered"
+        name="joinGameCode"
+        id="joinGameCode"
+      />
+      <button className={"btn btn-square" + (buttonLoading ? " loading" : "")} type="submit">
+        Join
+      </button>
+    </form>
+  );
+}
