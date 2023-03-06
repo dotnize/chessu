b98e4a23a91bbe90047a7c6cf2b3968bd43509f8
Nathaniel Tampus <contact@nize.ph>
2023-03-06 17:25:15 +0800

    redirect on client for 404

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,12 +1,14 @@
-export default function NotFound() {}
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 // Temporary. Move to app/ directory when it's supported
 
-export function getStaticProps() {
-  return {
-    redirect: {
-      destination: "/",
-      permanent: true
-    }
-  };
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  });
+
+  return null;
 }
