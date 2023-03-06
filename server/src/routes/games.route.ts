c548f10d6396a7995fc29544e0beeee104b8b3b3
Nathaniel Tampus <contact@nize.ph>
2023-03-06 15:10:00 +0800

    cleanup comment

--- server/src/routes/games.route.ts
+++ server/src/routes/games.route.ts
@@ -7,6 +7,4 @@ router.route("/").get(controller.getActivePublicGames).post(controller.createGam
 
 router.route("/:code").get(controller.getActiveGame);
 
-// todo: api for updating games/moves requiring authentication
-
 export default router;
