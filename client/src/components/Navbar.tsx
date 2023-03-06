045470b3983e8d4904cc4764b30923fc2ffddc12
Nathaniel Tampus <contact@nize.ph>
2023-03-06 11:23:36 +0800

    navbar site name changes - removed link for site name - added tooltip dropdown for alpha badge

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,36 @@
 import { IconUser, IconSun, IconMoon } from "@tabler/icons-react";
-import Link from "next/link";
 
 export default function Navbar() {
   return (
     <header className="navbar mx-1 w-auto justify-center drop-shadow-sm md:mx-16 lg:mx-40">
       <div className="flex-1">
-        <Link
-          href="/"
-          className="btn btn-ghost no-animation gap-1 text-xl normal-case hover:bg-transparent"
-        >
+        <span className="btn btn-ghost no-animation cursor-default gap-1 text-xl normal-case hover:bg-transparent">
           chessu
-          <span className="badge badge-sm mt-1">alpha</span>
-        </Link>
+          <div className="dropdown">
+            <label tabIndex={0} className="badge badge-sm mt-1 cursor-help">
+              alpha
+            </label>
+            <div
+              tabIndex={0}
+              className="dropdown-content card card-compact bg-primary text-primary-content w-64 shadow"
+            >
+              <div className="card-body cursor-default">
+                <p className="text-left text-xs">
+                  This project is a work in progress. You can view the roadmap{" "}
+                  <a
+                    href="https://github.com/users/nizewn/projects/2"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="link"
+                  >
+                    here
+                  </a>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+        </span>
       </div>
       <div className="flex-none">
         <div className="btn btn-ghost btn-circle">
