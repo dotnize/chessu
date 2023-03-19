c6bd485d6137c5bad18aaec2166a886dead39eef
Nathaniel Tampus <contact@nize.ph>
2023-03-19 21:23:15 +0800

    update game data on when updating guest names

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -7,6 +7,9 @@ import { activeGames } from "../db/models/game.model.js";
 import { io } from "../server.js";
 import * as UserModel from "../db/models/user.model.js";
 
+import { activeGames } from "../db/models/game.model.js";
+import { io } from "../server.js";
+
 export const getCurrentSession = async (req: Request, res: Response) => {
     try {
         if (req.session.user) {
