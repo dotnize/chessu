d01bb43004cd72f7d60887629e5e4ee5fdb8977d
Nathaniel Tampus <contact@nize.ph>
2023-04-05 22:34:53 +0800

    update archive notfound message

--- client/src/app/archive/[id]/not-found.tsx
+++ client/src/app/archive/[id]/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <div className="text-2xl font-bold">Error 404</div>
-      <div className="text-xl">Not found</div>
+      <div className="text-xl">Game not found</div>
     </div>
   );
 }
