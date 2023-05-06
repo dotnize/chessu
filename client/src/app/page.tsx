f5d5557f9ecccc410fc392d1e20c2f0a11572860
Nathaniel Tampus <contact@nize.ph>
2023-05-06 20:52:26 +0800

    fix build errors on root page set revalidate to 0 since PublicGames is already dynamic

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,8 @@ import PublicGames from "@/components/home/PublicGames/PublicGames";
 import JoinGame from "@/components/home/JoinGame";
 import CreateGame from "@/components/home/CreateGame";
 
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <div className="flex w-full flex-wrap items-center justify-center gap-8 px-4 py-10 lg:gap-16 ">
