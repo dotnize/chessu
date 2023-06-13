032fd8bf24b398581871e233e12deac8b2cce209
Nathaniel Tampus <contact@nize.ph>
2023-06-13 12:48:34 +0800

    remove unused @ts-expect-error

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,12 @@
-import PublicGames from "@/components/home/PublicGames/PublicGames";
-import JoinGame from "@/components/home/JoinGame";
 import CreateGame from "@/components/home/CreateGame";
+import JoinGame from "@/components/home/JoinGame";
+import PublicGames from "@/components/home/PublicGames/PublicGames";
 
 export const revalidate = 0;
 
 export default function Home() {
   return (
     <div className="flex w-full flex-wrap items-center justify-center gap-8 px-4 py-10 lg:gap-16 ">
-      {/* @ts-expect-error Server Component */}
       <PublicGames />
 
       <div className="flex flex-col items-center gap-4">
