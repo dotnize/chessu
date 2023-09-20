c0842827aa8fd0a8e45b60202a3ba754793fd692
Nathaniel Tampus <nathaniel@n9ze.com>
2023-09-20 17:04:44 +0800

    copy global styles and layout from src

---
+++ client/app/layout.tsx
@@ -0,0 +1,64 @@
+import "./globals.css";
+
+import type { ReactNode } from "react";
+
+// import Footer from "~/components/Footer";
+// import Header from "~/components/Header";
+
+import ContextProvider from "~/context/ContextProvider";
+
+export const metadata = {
+  title: "chessu",
+  description: "Play Chess online.",
+  openGraph: {
+    title: "chessu",
+    description: "Play Chess online.",
+    url: "https://ches.su",
+    siteName: "chessu",
+    locale: "en_US",
+    type: "website"
+  },
+  robots: {
+    index: true,
+    follow: false,
+    nocache: true,
+    noarchive: true
+  },
+  icons: {
+    icon: [
+      { type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+      { type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" }
+    ],
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" }
+  },
+  manifest: "/site.webmanifest",
+  metadataBase: new URL(process.env.VERCEL ? "https://ches.su" : "http://localhost:3000")
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ContextProvider>
+          {/* <Header /> */}
+
+          <main>{children}</main>
+        </ContextProvider>
+
+        {/* <Footer /> */}
+
+        {/* next/script issue: https://github.com/vercel/next.js/issues/43402 */}
+        <script
+          id="load-theme"
+          dangerouslySetInnerHTML={{
+            __html: `if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+              document.documentElement.classList.add("dark");
+          } else {
+              document.documentElement.classList.remove("dark");
+          }`
+          }}
+        ></script>
+      </body>
+    </html>
+  );
+}
