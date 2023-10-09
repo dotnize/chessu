989a390726540544afa660b7cfe523cd76ca4e37
Nathaniel Tampus <nate@mugnavo.com>
2023-10-09 12:20:52 +0800

    remove old frontend files

--- client/src/config.ts
+++
@@ -1,2 +0,0 @@
-// back-end server url
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
--- client/src/context/session.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from "@chessu/types";
-import { createContext, Dispatch, SetStateAction } from "react";
-
-export const SessionContext = createContext<{
-    user: User | null | undefined; // undefined = hasn't been checked yet, null = no user
-    setUser: Dispatch<SetStateAction<User | null>>;
-} | null>(null);
