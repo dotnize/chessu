d0abedd4b88f15f8d7fbade6101bdf6a214d6850
Nathaniel Tampus <nate@mugnavo.com>
2024-12-24 22:00:18 +0800

    init db schema

--- lib/server/db/schema.ts
+++ lib/server/db/schema.ts
@@ -1,4 +1,11 @@
-import { integer, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -15,6 +22,11 @@ export const user = pgTable("user", {
     .$onUpdate(() => new Date()),
   setup_at: timestamp(),
   terms_accepted_at: timestamp(),
+
+  wins: integer().default(0).notNull(),
+  losses: integer().default(0).notNull(),
+  draws: integer().default(0).notNull(),
+  // rating/elo?
 });
 
 export const oauthAccount = pgTable(
@@ -40,5 +52,32 @@ export const session = pgTable("session", {
   }).notNull(),
 });
 
+export const endReasonEnum = pgEnum("end_reason", [
+  "checkmate",
+  "resign",
+  "timeout",
+  "draw",
+  "abandoned",
+]);
+
+// a finished chess game. ongoing games will be stored in redis
+export const game = pgTable("game", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  started_at: timestamp().notNull(),
+  ended_at: timestamp().notNull().defaultNow(),
+  end_reason: endReasonEnum().notNull(),
+  pgn: text().notNull(),
+
+  winner_id: integer().references(() => user.id),
+  white_id: integer().references(() => user.id),
+  black_id: integer().references(() => user.id),
+
+  // in case user is a guest
+  winner_name: text(),
+  white_name: text(),
+  black_name: text(),
+});
+
 export type User = typeof user.$inferSelect;
 export type Session = typeof session.$inferSelect;
+export type Game = typeof game.$inferSelect;
