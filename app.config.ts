f472c531b9c291830311dd81da2816c4b6cae0ee
Nathaniel Tampus <nate@mugnavo.com>
2025-02-01 19:38:22 +0800

    replace postcss with tailwindcss/vite

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-// import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -8,7 +8,7 @@ export default defineConfig({
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
-      // tailwindcss(),
+      tailwindcss(),
     ],
   },
 
